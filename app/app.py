1a123f4224d09fabcad52d9762612f5fdb04e0c9
ANAGHANU <anaghanu448@gmail.com>
2026-08-13 08:37:54 +0530

    previous_tag  testing

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps AWS CI/CD application is running:v2!"
+    return "DevOps AWS CI/CD application is running"
 
 
 @app.route("/health")
